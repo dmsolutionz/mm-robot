6719ce5064b4f12e92b7d9f383fac372cd36a421
Daniel McClure <daniel@dmsolutions.io>
2019-11-08 01:04:18 +0000

    feat(github): documents support

---
+++ docs/precache-manifest.e5a8f4b0a1ef06c39953c7e38fce045c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469c80f2be593cbb4f19384b28459659",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ae8a62683f5ddb7854",
+    "url": "/static/css/main.aea2b186.chunk.css"
+  },
+  {
+    "revision": "f221ad2f17e7aa799205",
+    "url": "/static/js/2.c4cfbb0c.chunk.js"
+  },
+  {
+    "revision": "85ae8a62683f5ddb7854",
+    "url": "/static/js/main.f6d20da9.chunk.js"
+  },
+  {
+    "revision": "605d5806d3da360fa68e",
+    "url": "/static/js/runtime-main.ff07df9a.js"
+  }
+]);
